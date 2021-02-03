5369d846b368c28fb0371439f1dd7b0d79fa39c2
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-03 17:37:06 +0100

    add state object to parent App component,  update with fetched data from Youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
-        })
+        });
+        this.setState( {videos: response.data.items} );
+        console.log(this.state.videos)
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
+                I have number of {this.state.videos.length} videos in my list
             </div>
         )
     }
