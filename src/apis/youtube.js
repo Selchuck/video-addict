c19d6b13e784d34ad5d3e8a3c7a3315bc353463c
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-03 11:15:32 +0100

    install axios library into project  and configure YouTube API instance

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCIu_MsXSiM0mKOu8HG1Eez6SCynbHKZEs';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
